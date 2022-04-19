ca007b684ca745a44b37f18a694c6ea466630488
unknown <vladimirmomiroski@gmail.com>
2022-04-19 09:56:41 +0200

    setting backend basic structure

---
+++ backend/InitialTodos.js
@@ -0,0 +1,18 @@
+export const TodoArr = [
+    {
+      id: 1,
+      name: "Gym",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      name: "Running",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      name: "Dota :D",
+      isCompleted: false,
+    },
+  ];
+  
