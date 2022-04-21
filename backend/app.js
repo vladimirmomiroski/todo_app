da98edcf16896be03ae795a1ee033929787d0943
unknown <vladimirmomiroski@gmail.com>
2022-04-21 12:51:48 +0200

    the crud operation with the mongoDB is functioning, the error about header is still there

--- backend/app.js
+++ backend/app.js
@@ -2,100 +2,93 @@ const express = require("express");
 const { mongoose } = require("mongoose");
 const cors = require("cors");
 const port = 5000;
-const Todo = require('./models/Todos')
-require("dotenv/config")
+const Todo = require("./models/Todos");
+require("dotenv/config");
 
 const app = express();
 app.use(express.json());
-
-let todoArr = [
-  {
-    id: 1,
-    name: "Gym",
-    isCompleted: false,
-  },
-  {
-    id: 2,
-    name: "Running",
-    isCompleted: false,
-  },
-  {
-    id: 3,
-    name: "Coding",
-    isCompleted: false,
-  },
-  {
-    id: 4,
-    name: "Dota",
-    isCompleted: false,
-  },
-];
 
 // Middlewares
 app.use(cors());
 
 // routes
 app.get("/", (request, response) => {
- Todo.find()
- .then(data => response.json(data))
+  response.send(todoArr);
 });
 
 app.get("/todos", (request, response) => {
-  response.json(todoArr);
+  Todo.find().then((data) => response.json(data));
 });
 
 // working in progress
 app.post("/todos", (request, response) => {
   const todo = request.body;
-  console.log(todo.name)
-const post = new Todo({
+  console.log(todo.name);
+  const post = new Todo({
     name: todo.name,
-    isCompleted: todo.isCompleted
-})
-console.log(post)
+    isCompleted: todo.isCompleted,
+  });
+  console.log(post);
 
-  post.save()
-  .then(data => {
-    response.json(data)
-  })
-  .catch(error => {
-    console.log(error)
-  })
-  
+  post
+    .save()
+    .then((data) => {
+      response.json(data);
+      response.setHeader('Content-Type', 'text/plain');
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
   response.json({
     status: "success",
   });
 });
 
-app.delete("/todos/:id", (request, response) => {
+app.delete("/todos/:id", async (request, response) => {
   const { id } = request.params;
-  todoArr = todoArr.filter((el) => el.id !== +id);
+  console.log(id);
+  try {
+    const removedTodo = await Todo.deleteOne({ _id: id });
+    response.json(removedTodo);
+    response.setHeader('Content-Type', 'text/plain');
+  } catch (error) {
+    response.json({ message: error });
+  }
+
+
   response.json({
     status: "success",
   });
 });
 
-app.patch("/todos/:id", (request, response) => {
+app.patch("/todos/:id", async (request, response) => {
   const { id } = request.params;
-  console.log(id + "patch");
-  todoArr = todoArr.map((el) => {
-    if (el.id === +id) {
-      el.isCompleted = !el.isCompleted;
-      return el;
-    }
-    return el;
-  });
+  console.log(id);
+  try {
+    const updatedTodo = await Todo.updateOne(
+      { _id: id },
+      { $set: { isCompleted: true} }
+    );
+    response.json(updatedTodo);
+    response.setHeader('Content-Type', 'text/plain');
+  } catch (error) {
+    response.json({ message: error });
+  }
+
+
   response.json({
     status: "success",
   });
 });
 
-// connect to DB
-mongoose.connect(
-  process.env.DB_CONNECTION_URL,
-  { userNewUrlParser: true },
-  () => console.log("connected")
-);
+mongoose
+  .connect(process.env.DB_CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Database connected!"))
+  .catch((err) => console.log(err));
 
 // listening to the server
 app.listen(port);
