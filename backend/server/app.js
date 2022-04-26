4a0027fab5fbfa35e9e43bebf2bdc9eef38c0ed9
unknown <vladimirmomiroski@gmail.com>
2022-04-26 19:46:30 +0200

    implemented prettier in backend directory

--- backend/server/app.js
+++ backend/server/app.js
@@ -1,9 +1,9 @@
-const { mongoose } = require('mongoose');
-const express = require('express');
-const cors = require('cors');
-const Todo = require('../server/models/Todos');
+const { mongoose } = require("mongoose");
+const express = require("express");
+const cors = require("cors");
+const Todo = require("../server/models/Todos");
 
-require('dotenv/config');
+require("dotenv/config");
 const PORT = process.env.PORT;
 
 const app = express();
@@ -14,86 +14,71 @@ app.use(express.json());
 app.use(cors());
 
 // routes
-app.get('/', (request, response) => 
-{
-  response.send('Home Route');
+app.get("/", (request, response) => {
+  response.send("Home Route");
 });
 
-app.get('/todos', (request, response) => 
-{
+app.get("/todos", (request, response) => {
   Todo.find()
-    .then(data => response.json(data))
-    .catch(error => 
-    {
-    	throw new Error(error);
+    .then((data) => response.json(data))
+    .catch((error) => {
+      throw new Error(error);
     });
 });
 
-app.post('/todos', (request, response) => 
-{
-	const todo = request.body;
+app.post("/todos", (request, response) => {
+  const todo = request.body;
 
-	const post = new Todo({
+  const post = new Todo({
     name: todo.name,
-    isCompleted: todo.isCompleted
+    isCompleted: todo.isCompleted,
   });
 
   post
     .save()
-    .then(data => 
-    {
+    .then((data) => {
       response.json(data);
     })
-    .catch(error => 
-    {
-    	throw new Error(error);
+    .catch((error) => {
+      throw new Error(error);
     });
 });
 
-app.delete('/todos/:id', async(request, response) => 
-{
-	const { id } = request.params;
+app.delete("/todos/:id", async (request, response) => {
+  const { id } = request.params;
 
-	try 
-	{
-		const removedTodo = await Todo.deleteOne({ _id: id });
+  try {
+    const removedTodo = await Todo.deleteOne({ _id: id });
 
     response.json(removedTodo);
-	}
-	catch (error) 
-	{
+  } catch (error) {
     response.json({ message: error });
-	}
+  }
 });
 
-app.patch('/todos/:id', async(request, response) => 
-{
-	const { id } = request.params;
+app.patch("/todos/:id", async (request, response) => {
+  const { id } = request.params;
 
-	try 
-	{
-		const updatedTodo = await Todo.updateOne(
+  try {
+    const updatedTodo = await Todo.updateOne(
       { _id: id },
       { $set: { isCompleted: true } }
-		);
+    );
 
     response.json(updatedTodo);
-	}
-	catch (error) 
-	{
+  } catch (error) {
     response.json({ message: error });
-	}
+  }
 });
 
 // db connection
 mongoose
   .connect(process.env.DB_CONNECTION_URL, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
-  .catch(error => 
-  {
-  	throw new Error(error);
+  .catch((error) => {
+    throw new Error(error);
   });
 
 // listening to the server
